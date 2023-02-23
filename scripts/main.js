a003c9bc683c7d45ed05f0bd3196f86b435f0b95
rjbray915 <61060963+rjbray915@users.noreply.github.com>
2023-02-23 00:09:37 -0500

    new and improved

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,42 @@
+function setUserName(){
+    const new_name = prompt("Please enter your name");
+    //check if new name is empty
+    if(!new_name){
+        setUserName();
+    }
+    else{
+        localStorage.setItem("name", new_name);
+        myHeading.textContent = `COSC 302/307, ${new_name}`;
+    }
+    
+}
+
+//variables
+const myButton = document.querySelector("button");
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "COSC 302/307";
 
-let x = 35
-let y = '26'
+// set name based on local storage status
+if (!localStorage.getItem("name")){
+    setUserName();
+}
+else{
+    const stored_name = localStorage.getItem("name");
+    myHeading.textContent = `COSC 302/307, ${stored_name}`;
+}
 
-console.log(x + y)
+//set onclick for changing the user
+myButton.onclick = () => {
+    setUserName();
+};
+
+const myImage = document.querySelector("img");
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if(mySrc === "../assets/images/Susremaster.webp"){
+        myImage.setAttribute("src", "../assets/images/me.jpg");
+    }
+    else{
+        myImage.setAttribute("src", "../assets/images/Susremaster.webp");
+    }
+}
